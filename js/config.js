ab31f244e64dad34f7e490ca4d201aecaa7c8672
Cristian-Morales2 <164056281+Cristian-Morales2@users.noreply.github.com>
2024-04-11 13:02:52 -0500

    Update config.js: added in userPoolId and userPoolClientId

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-1_hiIs9MOaB',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '30gr24ir5gv2p5e9e21teq7nrb',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-1'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
